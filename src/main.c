9fce99ea6e0714d2ce223805050be789b3453027
Dionisio Molano <dmolanor@gmail.com>
2021-08-05 08:55:14 +0200

    Ready

---
+++ src/main.c
@@ -0,0 +1,47 @@
+/* 
+ * This file is part of the Sipeed Longan Nano 2663 Initium.
+ * Copyright (c) 2021 Dionisio Molano Robledo.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main.h"
+
+// ---------------------------------------------------------------------
+// Private Constants
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// Private Variables
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// Private Prototypes
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// Bodies
+// ---------------------------------------------------------------------
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    int result = 0;
+
+    return result;
+}
+
+// ---------------------------------------------------------------------
+// Private Bodies
+// ---------------------------------------------------------------------
